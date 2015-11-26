a6ee3c94c802d4a790c279d3a74e642188d0937b
Luccan <luccan.ryanata@gmail.com>
2015-11-26 16:44:54 +0800

    Removed iframe border

--- js/lazyblogger.js
+++ js/lazyblogger.js
@@ -25,7 +25,7 @@ for (var i=0;i<url.length;i++){
 document.body.style["overflow"] = "hidden";
 document.body.style["margin"] = "0";
 //console.log(iframe_src);
-document.body.innerHTML = '<iframe id="myIframe" src="'+iframe_src+'" style="height:100%; width:100%;"></iframe>';
+document.body.innerHTML = '<iframe id="myIframe" src="'+iframe_src+'" style="height:100%; width:100%; border:none;"></iframe>';
 
 var myIframe = document.getElementById('myIframe');
 myIframe.onload = function() {
