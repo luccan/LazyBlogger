66743dc95c0a8c0a876f13187b4fb82ff2f67a29
Luccan <luccan.ryanata@gmail.com>
2015-11-26 09:24:38 +0800

    Initial Commit

---
+++ js/lazyblogger.js
@@ -0,0 +1,23 @@
+/* 	lazyblogger.js
+	The engine that loads _layout.html iframe into current body.
+	Assumes there exist a _layout.html at the parent folder of the content/ directory.
+	This should only be called by items in the content/ directory.
+*/
+var url = window.location.href.split('/');
+var j = 0;
+var iframe_src = './_layout.html';
+for (var i=0;i<url.length;i++){
+	if (j>0){ //append href
+		iframe_src = '../' + iframe_src + '/' + url[i]; //move up one level and append path to parameter
+		j++;
+	}
+	else if (url[i]=='content'){
+		j=1;
+		iframe_src += '?path=./content';
+	}
+}
+
+document.body.style["overflow"] = "hidden";
+document.body.style["margin"] = "0";
+//console.log(iframe_src);
+document.body.innerHTML = '<iframe src="'+iframe_src+'" height="100%" width="100%"></iframe>'
